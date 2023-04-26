2c27ee4f4649920885b9fd9d9200c661c7cc6d27
zewelrey <38527917+jiweiyx@users.noreply.github.com>
2023-04-26 09:51:54 +0800

    修改了地址
    简化了获取程序

--- setting.py
+++ setting.py
@@ -1,13 +1,16 @@
 #telegram bot token
-TOKEN = '5893744202:AAEWLjgwcmSgBu1xrJHVnL9p1HEZnIgeIVo'
+TOKEN = '5486577393:AAH1tTvZ54iNf9NJMaabgfUSwqzhsUsfBtY'
 
 #ton account
-ACCOUNT = 'EQAd3b5PyiksK5Uizi8azpd4fw6IJ8HDrIUEcsyAVXjG0uV8'
-wallet_mnemonics = ['example', 'battle', 'patient', 'hidden', 'afraid', 'annual', 'plug', 'ceiling', 'suspect', 'harbor', 'science',
-                        'hospital', 'state', 'horse', 'advance', 'radio', 'appear', 'such', 'share', 'cabbage', 'dune', 'pyramid', 'clock', 'add']
+ACCOUNT = 'EQCVgKLjByZUitJ1AkRuZPcK3PMh54aHeF16K7LIfyvQK1CM'
+wallet_mnemonics = ['blur','balcony','kitchen','fox','animal','purpose','jewel','buyer','toilet','audit','sting','pioneer',
+                        'gaze','chuckle','jungle','dolphin','tiger','opera','puppy','tissue','reduce','frown','horse','hungry']
 #tonClient
 
 tonclient_url = 'https://ton.org/testnet-global.config.json'
+
+#tonCenter API
+toncenter_key = "1e66fdeb4e2f7376c0a107cb99c6f506890dd4d607795f2d18be08c8438f7189"
 
 #RpidAPI KEY
 RapidAPI_Key = "ff8125e52cmsh0fbe0b3d02192b0p1ae750jsn6198c7e6082a"
