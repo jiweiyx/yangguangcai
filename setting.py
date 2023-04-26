23788eb99e4f3773fc345dd088fbac705cc6d56a
zewelrey <38527917+jiweiyx@users.noreply.github.com>
2023-04-26 14:02:47 +0800

    删除原来的toncenter和rappyapi的接口

--- setting.py
+++ setting.py
@@ -8,9 +8,3 @@ wallet_mnemonics = ['blur','balcony','kitchen','fox','animal','purpose','jewel',
 #tonClient
 
 tonclient_url = 'https://ton.org/testnet-global.config.json'
-
-#tonCenter API
-toncenter_key = "1e66fdeb4e2f7376c0a107cb99c6f506890dd4d607795f2d18be08c8438f7189"
-
-#RpidAPI KEY
-RapidAPI_Key = "ff8125e52cmsh0fbe0b3d02192b0p1ae750jsn6198c7e6082a"
